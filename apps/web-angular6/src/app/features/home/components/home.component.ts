d39f11b464efbc5b7a0bef13b1c6ea75bf4ddfa8
ramprit <Rampritsahani@outlook.in>
2018-07-14 08:45:34 +0530

    updated

---
+++ apps/web-angular6/src/app/features/home/components/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import { BaseComponent } from '@myworkspace/core';
+
+@Component({
+  selector: 'foo-home',
+  templateUrl: 'home.component.html'
+})
+export class HomeComponent extends BaseComponent {}
